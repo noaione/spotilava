d74609ee16373b37e78951374a84bff382673ed6
noaione <noaione0809@gmail.com>
2021-12-13 17:57:02 +0800

    Add deezer classvar

--- internals/sanic.py
+++ internals/sanic.py
@@ -3,6 +3,7 @@ from typing import ClassVar, Optional
 from sanic import Sanic
 from sanic.blueprints import Blueprint
 
+from .deezer import DeezerClient
 from .spotify import LIBRESpotifyWrapper
 from .tidal import TidalAPI
 
@@ -10,12 +11,14 @@ from .tidal import TidalAPI
 class SpotilavaSanic(Sanic):
     spotify: ClassVar[Optional[LIBRESpotifyWrapper]]
     tidal: ClassVar[Optional[TidalAPI]]
+    deezer: ClassVar[Optional[DeezerClient]]
     chunk_size: ClassVar[int] = 4096
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.spotify = None
         self.tidal = None
+        self.deezer = None
 
 
 class SpotilavaBlueprint(Blueprint):
