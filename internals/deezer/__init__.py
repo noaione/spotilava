ca4378e53e743e4dd599c45e2389f29d1c774af8
noaione <noaione0809@gmail.com>
2021-12-13 17:56:51 +0800

    [wip] Implement Deezer

---
+++ internals/deezer/__init__.py
@@ -0,0 +1,13 @@
+"""
+internals.deezer
+~~~~~~~~~~~~~~~~~
+The handler for Deezer
+
+:copyright: (c) 2021-present noaione
+:license: MIT, see LICENSE for more details.
+"""
+
+# flake8: noqa
+
+from .client import *
+from .models import *
